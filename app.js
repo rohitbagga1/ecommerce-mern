a3a536aa1ee08bc2b1693fd1e58222a5e96ca076
rohit <you@example.com>
2023-07-06 00:42:59 +0530

    imported package bogy parser, morgan to parse body request from cliet side made operation to user model

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 // import routes
 const userRoutes = require("./routes/user");
@@ -9,9 +12,13 @@ const app = express();
 
 // db
 mongoose
-    .connect(process.env.DATABASE, {
-    })
+    .connect(process.env.DATABASE, {})
     .then(() => console.log("DB Connected"));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // routes middleware
 app.use("/api", userRoutes);
