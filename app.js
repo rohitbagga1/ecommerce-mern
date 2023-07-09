42bc53418df20043c184de2c6fef508d71363a4f
rohit <you@example.com>
2023-07-09 21:52:03 +0530

    Installed express validator for custom error handeling messages in validator dir - index.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+
 require("dotenv").config();
 // import routes
 const userRoutes = require("./routes/user");
@@ -19,6 +21,7 @@ mongoose
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use("/api", userRoutes);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup } = require("../controllers/user");
 
-router.post("/signup", signup);
+const { signup } = require("../controllers/user");
+const { userSignupValidator } = require("../validator");
+
+router.post("/signup", userSignupValidator, signup);
 
 module.exports = router;
+
+
+
+
