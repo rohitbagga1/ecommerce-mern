a9934cc88de1828d94de62f965d397bf6f178a71
rohit <you@example.com>
2023-07-09 21:39:45 +0530

    Added a way to handel errors in helper folder

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,20 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
     console.log("req.body", req.body);
-    const user = new User(req.body);
-    user.save((err, user) => {
-        if (err) {
-            return res.status(400).json({
-                error
-            });
-        }
+    try {
+        const user = new User(req.body);
+        const savedUser = await user.save();
         res.json({
-            user
+            user: savedUser
         });
-    });
+    } catch (error) {
+        let msg =  errorHandler(error)
+        console.log('kfkjhdkhsf');
+        console.log(error);
+        res.status(400).json({
+            msg
+        });
+    }
 };
