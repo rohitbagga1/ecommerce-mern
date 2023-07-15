119e711bacc51bc5f97a44bfe1ec7bb6792c776a
rohit <you@example.com>
2023-07-16 00:32:15 +0530

    Added validations to product controller for required products and image size

--- controllers/product.js
+++ controllers/product.js
@@ -13,6 +13,30 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
+
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
+
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+        
          // Convert array values to strings
         for (let field in fields) {
             if (Array.isArray(fields[field])) {
@@ -25,6 +49,11 @@ exports.create = (req, res) => {
         let product = new Product(fields);
         // console.log('check the data',product);
         if (files.photo) {
+            if (files.photo[0].size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo[0].filepath);
             product.photo.contentType = files.photo[0].type;
         }
