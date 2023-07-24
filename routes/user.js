7c80fe077f095b47a97b1038fb6710badd914d4b
rohit <you@example.com>
2023-07-24 23:10:30 +0530

    Made user crud

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,18 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { userById } = require("../controllers/user");
+
+const { userById, read, update  } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, (req, res) => {
     res.json({
-        user: req.profile,
+        user: req.profile, 
         test: req.auth
     });
 });
 
 router.param("userId", userById);
+router.get("/user/:userId", requireSignin, isAuth, read);
+router.put("/user/:userId", requireSignin, isAuth, update);
 
 module.exports = router;
