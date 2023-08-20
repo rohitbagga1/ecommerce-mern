278ca9455af2885740aea587575fe0947efa72e3
rohit <you@example.com>
2023-08-20 22:54:42 +0530

    show products in card

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
-
+const cors = require("cors");
 require("dotenv").config();
 // import routes
 const authRoutes = require("./routes/auth");
@@ -25,7 +25,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
-
+app.use(cors());
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
